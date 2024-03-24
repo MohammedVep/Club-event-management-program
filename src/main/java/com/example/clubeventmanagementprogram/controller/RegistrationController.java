e3c6006bbd3b4dbd04f3da76b0229733076fc9e5
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-03-24 06:56:17 -0400

    Fixed Code and created registration controller

---
+++ src/main/java/com/example/clubeventmanagementprogram/controller/RegistrationController.java
@@ -0,0 +1,77 @@
+package com.example.clubeventmanagementprogram.controller;
+
+import com.example.clubeventmanagementprogram.model.Club;
+import com.example.clubeventmanagementprogram.model.User;
+import com.example.clubeventmanagementprogram.service.UserService;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class RegistrationController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField emailField;
+
+    private UserService userService;
+
+    private ObservableList<User> users;
+
+    @FXML
+    private Button registerButton;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @FXML
+    public void initialize() {
+        registerButton.setOnAction(this::register);
+    }
+
+    @FXML
+    public void register(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        String email = emailField.getText();
+        int id = users.size() + 1;
+        // Validation checks here - make sure fields are not empty etc.
+        User user = new User(id ,username, password,email);
+        user.setUserName(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        try {
+            userService.addUser(user);
+            showMessage("Registration successful!");
+            // Clear the input fields or navigate to another screen
+            // Load Home page view
+            Parent homePageParent = FXMLLoader.load(getClass().getResource("/Users/mohammedvepari/IdeaProjects/Club-event-management-program/src/main/resources/com/example/clubeventmanagementprogram/home-view.fxml"));
+            Scene homePageScene = new Scene(homePageParent);
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(homePageScene);
+            appStage.show();
+        } catch (Exception e) {
+            // Handle exceptions properly, inform the user
+            showMessage("Registration failed: " + e.getMessage());
+        }
+    }
+
+    private void showMessage(String message) {
+        // Display this message to the user
+
+    }
+}
